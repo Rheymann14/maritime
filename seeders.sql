687c3ddb43aa071216525da1c47c01b1036f5347
tiiimm <onrubia.fatima98@gmail.com>
2024-06-28 13:30:47 +0800

    stable

---
+++ seeders.sql
@@ -0,0 +1,7 @@
+INSERT INTO `mheis`(`institutional_code`, `school_name`, `school_type`, `region`, `address`, `email`, `contact_number`, `created_by`, `updated_by`) VALUES ('1234', 'Jose Rizal Memorial State University', 'PUBLIC', 'IX', 'Dapitan City, Zamboanga del Norte', 'none', 'none', 1, 1);
+INSERT INTO `mheis`(`institutional_code`, `school_name`, `school_type`, `region`, `address`, `email`, `contact_number`, `created_by`, `updated_by`) VALUES ('2345', 'Our Lady of Triumph Institute of Technology Inc.', 'PRIVATE', 'IX', 'Pagadian City, Zamboanga Del Sur', 'none', 'none', 1, 1);
+INSERT INTO `mheis`(`institutional_code`, `school_name`, `school_type`, `region`, `address`, `email`, `contact_number`, `created_by`, `updated_by`) VALUES ('3456', 'Zamboanga Peninsula Polytechnic State University', 'PUBLIC', 'IX', 'R.T. Lim Boulevard, Zamboanga City', 'none', 'none', 1, 1);
+INSERT INTO `mheis`(`institutional_code`, `school_name`, `school_type`, `region`, `address`, `email`, `contact_number`, `created_by`, `updated_by`) VALUES ('9096', 'Zamboanga State College of Marine Sciences and Technology', 'PUBLIC', 'IX', 'Fort Pilar,  Zamboanga City', 'none', 'none', 1, 1);
+
+
+
